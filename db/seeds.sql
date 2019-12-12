32d3e61172a92b571521377d84dbd6f2986e71c3
Robert Moore <rcmoore3@ncsu.edu>
2019-12-12 10:04:57 -0500

    seeds added

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name, devoured) VALUES ('Angus Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Agnes Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Thiqq Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Black Bean Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Cabbage Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Soylent Burger', false);
+INSERT INTO burgers (name, devoured) VALUES ('Super Mega Chocolatey Fudge Coated Burger', false);
