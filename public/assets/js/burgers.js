d9668172df52d5cd006c5d26897a63f43252b61a
Robert Moore <rcmoore3@ncsu.edu>
2019-12-13 14:27:05 -0500

    updates burger.js

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,20 +1,27 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
-  $(".change-sleep").on("click", function(event) {
+  // $(".change-sleep").on("click", function(event) {
+  $(".change-devour").on("click", function(event) {
     var id = $(this).data("id");
-    var newSleep = $(this).data("newsleep");
+    var newDevour = $(this).data("newDevour");
+    // var newSleep = $(this).data("newsleep");
 
-    var newSleepState = {
-      sleepy: newSleep
+    var newDevourState = {
+      devour: newDevour
+      // var newSleepState = {
+      //   sleepy: newSleep
     };
 
     // Send the PUT request.
-    $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
+    // $.ajax("/api/cats/" + id, {
       type: "PUT",
-      data: newSleepState
+      data: newDevourState
+      // data: newSleepState
     }).then(
       function() {
-        console.log("changed sleep to", newSleep);
+        // console.log("changed sleep to", newSleep);
+        console.log("changed sleep to", newDevour);
         // Reload the page to get the updated list
         location.reload();
       }
@@ -25,33 +32,40 @@ $(function() {
     // Make sure to preventDefault on a submit event.
     event.preventDefault();
 
-    var newCat = {
+    var newBurger = {
+      // var newCat = {
       name: $("#ca").val().trim(),
-      sleepy: $("[name=sleepy]:checked").val().trim()
+      devour: $("[name=devour]:checked").val().trim()
+      // sleepy: $("[name=sleepy]:checked").val().trim()
     };
 
     // Send the POST request.
-    $.ajax("/api/cats", {
+    $.ajax("/api/burgers", {
+      // $.ajax("/api/cats", {
       type: "POST",
-      data: newCat
+      data: newBurger
+      // data: newCat
     }).then(
       function() {
-        console.log("created new cat");
+        console.log("created new burger");
         // Reload the page to get the updated list
         location.reload();
       }
     );
   });
 
-  $(".delete-cat").on("click", function(event) {
+  // $(".delete-cat").on("click", function(event) {
+  $(".delete-burger").on("click", function(event) {
     var id = $(this).data("id");
 
     // Send the DELETE request.
-    $.ajax("/api/cats/" + id, {
+    // $.ajax("/api/cats/" + id, {
+    $.ajax("/api/burgers/" + id, {
       type: "DELETE"
     }).then(
       function() {
-        console.log("deleted cat", id);
+        // console.log("deleted cat", id);
+        console.log("deleted burger", id);
         // Reload the page to get the updated list
         location.reload();
       }
