fac6fe2804918c12e99bf35e493d9c2e125a342f
Robert Moore <rcmoore3@ncsu.edu>
2019-12-13 14:52:14 -0500

    spun up server

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,7 +1,7 @@
-INSERT INTO burgers (name, devoured) VALUES ('Angus Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Agnes Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Thiqq Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Black Bean Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Cabbage Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Soylent Burger', false);
-INSERT INTO burgers (name, devoured) VALUES ('Super Mega Chocolatey Fudge Coated Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Angus Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Agnes Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Thiqq Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Bean Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cabbage Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Soylent Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Super Mega Chocolatey Fudge Coated Burger', false);
